9d949bfecb1ba6bcfb2a0340884fba8997b150fc
xPaw <github@xpaw.ru>
2013-12-17 00:58:15 +0200

    Init

---
+++ js/RemoteClient.js
@@ -0,0 +1,139 @@
+/*
+	"CRemoteControlHTTPReqStatic",      ".*", "GET",      "/static/(.*)"
+	"CRemoteControlHTTPReqButtonPress", ".*", "POST",     "/steam/button/([^/]*)/*"
+	"CRemoteControlHTTPReqKeyboard",    ".*", "POST",     "/steam/keyboard/([^/]*)/*"
+	"CRemoteControlHTTPReqMouse",       ".*", "POST",     "/steam/mouse/([^/]*)/*"
+	"CRemoteControlHTTPReqGames",       ".*", "GET",      "/steam/games/*"
+	"CRemoteControlHTTPReqGameAction",  ".*", "POST",     "/steam/games/(\\d+)/([^/]*)"
+	"CRemoteControlHTTPReqSpace",       ".*", "POST|GET", "/steam/space/*"
+	"CRemoteControlHTTPReqAuthorized",  ".*", "GET",      "/steam/authorized/*"
+*/
+
+var SteamRemoteClient =
+{
+	Address: location.protocol + '//' + location.host + '/steam/',
+	DeviceName: 'SteamDB Hacks',
+	DeviceToken: 'AmazingSteamDB',
+	AlertTimeout: 0,
+	
+	Keyboard:
+	{
+		Sequence: function( sequence )
+		{
+			SteamRemoteClient.DoPOST( 'keyboard/sequence/', { sequence: sequence } );
+		},
+		
+		Key: function( name )
+		{
+			SteamRemoteClient.DoPOST( 'keyboard/key/', { name: name } );
+		}
+	},
+	
+	Button:
+	{
+		Press: function( button )
+		{
+			SteamRemoteClient.DoPOST( 'button/' + button + '/' );
+		}
+	},
+	
+	Games:
+	{
+		Index: function( callback )
+		{
+			SteamRemoteClient.DoGET( 'games/', {}, callback );
+		},
+		
+		Action: function( appid, action )
+		{
+			SteamRemoteClient.DoPOST( 'games/' + appid + '/' + action );
+		}
+	},
+	
+	Space:
+	{
+		Current: function( callback )
+		{
+			SteamRemoteClient.DoGET( 'space/', {}, callback );
+		},
+		
+		Set: function( space )
+		{
+			SteamRemoteClient.DoPOST( 'space/', { name: space } ); // Known spaces: webbrowser, library, friends
+		}
+	},
+	
+	DoGET: function( url, additionalParameters, callback )
+	{
+		var data = jQuery.extend(
+		{
+			device_name: SteamRemoteClient.DeviceName,
+			device_token: SteamRemoteClient.DeviceToken
+		}, additionalParameters );
+		
+		jQuery.ajax( {
+			url: SteamRemoteClient.Address + url,
+			data: data,
+			method: 'GET',
+			dataType: 'json',
+			timeout: 5000,
+			success: function( response )
+			{
+				SteamRemoteClient.ShowAlert( 'success', '<b>Request Executed:</b> /' + url, data );
+				
+				if( typeof callback === 'function' )
+				{
+					callback( response );
+				}
+			},
+			error: function( )
+			{
+				SteamRemoteClient.ShowAlert( 'danger', '<b>Request Failed:</b> /' + url, data );
+			}
+		} );
+	},
+	
+	DoPOST: function( url, additionalParameters )
+	{
+		var data = jQuery.extend(
+		{
+			device_name: SteamRemoteClient.DeviceName,
+			device_token: SteamRemoteClient.DeviceToken
+		}, additionalParameters );
+		
+		jQuery.ajax( {
+			url: SteamRemoteClient.Address + url,
+			data: data,
+			method: 'POST',
+			dataType: 'json',
+			timeout: 5000,
+			success: function( )
+			{
+				SteamRemoteClient.ShowAlert( 'success', '<b>Request Executed:</b> /steam/' + url, data );
+			},
+			error: function( )
+			{
+				SteamRemoteClient.ShowAlert( 'danger', '<b>Request Failed:</b> /steam/' + url, data );
+			}
+		} );
+	},
+	
+	ShowAlert: function( cssClass, text, data )
+	{
+		$( '.alert' )
+			.removeClass( 'alert-' + ( cssClass === 'success' ? 'danger' : 'success' ) )
+			.addClass( 'alert-' + cssClass + ' in' )
+			.find( '.title' )
+			.html( text )
+			.end( )
+			.find( '.data' )
+			.text( data ? JSON.stringify( data ) : 'No payload.' );
+		
+		clearInterval( SteamRemoteClient.AlertTimeout );
+		
+		SteamRemoteClient.AlertTimeout = setTimeout( function( )
+		{
+			$( '.alert' ).removeClass( 'in' );
+		}, 2500 );
+	}
+};
---
+++ js/Shenanigans.js
@@ -0,0 +1,137 @@
+(function()
+{
+	var trapArea = $( '.mousetrap' ), input = $( '.js-steam-input' ), lastPosition = { x: 0.0, y: 0.0 };
+
+	// Key trap
+	$( document ).on( 'keydown', function( e )
+	{
+		if( trapArea.is( ':hover' ) )
+		{
+			e.preventDefault();
+			
+			var key = 'key_' + Keycode.GetValueByEvent( e );
+			
+			trapArea.find( 'h3' ).text( key );
+			
+			SteamRemoteClient.Keyboard.Key( key );
+		}
+	} );
+
+	$( document ).on( 'mousemove', function( e )
+	{
+		if( trapArea.is( ':hover' ) )
+		{
+			var deltaX = lastPosition.x - event.clientX,
+			    deltaY = lastPosition.y - event.clientY;
+			
+			lastPosition =
+			{
+				x: event.clientX,
+				y: event.clientY
+			};
+			
+			SteamRemoteClient.DoPOST( 'mouse/move',
+				{
+					delta_x: -deltaX,
+					delta_y: -deltaY
+				}
+			);
+		}
+	} );
+
+	$( trapArea ).on( 'click', function( e )
+	{
+		SteamRemoteClient.DoPOST( 'mouse/click', { button: 'mouse_left' } );
+	} );
+
+	// Get space
+	$( '.js-steam-get-space' ).click( function( e )
+	{
+		e.preventDefault( );
+		
+		SteamRemoteClient.Space.Current( function( data )
+		{
+			SteamRemoteClient.ShowAlert( 'success', '<b>Current space:</b> ' + data.data.name );
+		} );
+	} );
+
+	// Get list
+	$( '.js-steam-get-games' ).click( function( e )
+	{
+		e.preventDefault( );
+		
+		SteamRemoteClient.Games.Index( function( data )
+		{
+			var list = $( '.js-steam-games-list' ).empty( );
+			
+			console.log( data );
+			
+			for( var i in data.data )
+			{
+				var link = $( '<a></a>', { class: 'list-group-item', href: '#', 'data-appid': i } );
+				
+				var game = data.data[ i ];
+				
+				var name = game.name;
+				
+				if( game.installed )
+				{
+					name = '<b>' + name + '</b>';
+				}
+				else
+				{
+					name = '<i>' + name + ' (' + ( 0 | game.estimated_disk_bytes / 1000000 ) + ' MB)</i>';
+				}
+				
+				link.html( name ).click( function( e )
+				{
+					e.preventDefault( );
+					
+					input.val( $( this ).data( 'appid' ) );
+				} );
+				
+				list.append( link );
+			}
+			
+			if( list.is( ':empty' ) )
+			{
+				list.append( $( '<span></span>', { class: 'list-group-item' } ).text( 'The list is empty :(' ) );
+			}
+		} );
+	} );
+
+	$( '.js-steam-space' ).on( 'click', function( e )
+	{
+		e.preventDefault();
+		
+		SteamRemoteClient.Space.Set( input.val() );
+	} );
+
+	$( '.js-steam-sequence' ).on( 'click', function( e )
+	{
+		e.preventDefault();
+		
+		SteamRemoteClient.Keyboard.Sequence( input.val() );
+	} );
+
+	$( '.js-steam-key' ).on( 'click', function( e )
+	{
+		e.preventDefault();
+		
+		SteamRemoteClient.Keyboard.Key( input.val() );
+	} );
+
+	$( '.js-steam-button' ).on( 'click', function( e )
+	{
+		e.preventDefault();
+		
+		SteamRemoteClient.Button.Press( $( this ).data( 'button' ) || input.val() );
+	} );
+
+	$( '.js-steam-games' ).on( 'click', function( e )
+	{
+		e.preventDefault();
+		
+		SteamRemoteClient.Games.Action( input.val(), 'run' );
+	} );
+}());
